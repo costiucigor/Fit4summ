2df5e16a2caa80c15d73484aad1c765bca4889e0
Costiuc Igor <90152687+Xenilae@users.noreply.github.com>
2023-04-04 01:10:52 +0300

    added style

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Box, Stack, Typography} from "@mui/material";
+import {Box, Stack, Typography, Button} from "@mui/material";
+
+import BannerImage from "../assets/images/banner.png"
 
 const Banner = () => {
     return (
@@ -8,12 +10,29 @@ const Banner = () => {
             ml: {sm: "50px"}
         }}
              position="relative" p="20px">
-            <Typography fontWeight="600" fontSize="26px">
+            <Typography fontWeight="600" color={"#30B040"} fontSize="26px">
                 Fit4sum
             </Typography>
-            <Typography>
-                Get fit, grind <br /> Repeat
+            <Typography fontWeight={700}
+                        sx={{fontSize: {lg: "44px", xs: "40px"}}}
+            >
+                Get fit, grind <br/> Repeat
             </Typography>
+            <Button
+                sx={{
+                    backgroundColor: "#30B040",
+                    '&:hover': {
+                        backgroundColor: '#3DE952',
+                        href:"#exercises"
+                    },
+                }}
+                variant="contained"
+            >Checkout Exercises</Button>
+            <img
+                src={BannerImage}
+                alt="banner"
+                className="hero-banner-img"
+            />
         </Box>
     );
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -28,7 +28,7 @@ const NavBar = () => {
                 alignItems="flex-end"
             >
                 <Link to="/"
-                      style={{textDecoration: "none", color: "#3A1212", borderBottom: "3px solid #FF2625"}}>Home</Link>
+                      style={{textDecoration: "none", color: "#3A1212", borderBottom: "3px solid #3DE952"}}>Home</Link>
                 <a href="#exercises" style={{textDecoration: "none", color: "#3A1212"}}>Exercises</a>
             </Stack>
         </Stack>
