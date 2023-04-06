4a4e672ced63b045e38024d014523b54cb81ec55
Costiuc Igor <90152687+Xenilae@users.noreply.github.com>
2023-04-06 22:26:58 +0300

    fixed request

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,17 @@
 import React, {useEffect, useState} from 'react';
 
 import {Box, Button, Stack, TextField, Typography} from "@mui/material";
+import { exerciseOptions, fetchData} from "../utils/fetchData";
+
 
 const Search = () => {
     const [search, setSearch] = useState("");
 
     const handleSearch = async() => {
         if(search) {
-            // const exerciseData = await fetchData();
+            const exerciseData = await fetchData("https://exercisedb.p.rapidapi.com/exercises/bodyPartList", exerciseOptions);
+
+            console.log(exerciseData)
         }
     }
     return (
--- src/utils/fetchData.tsx
+++ src/utils/fetchData.tsx
@@ -1,3 +1,11 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+        'X-RapidAPI-Key': "4bd84654e9msh9034b208b00864bp139d91jsn643044d2afbd"
+    }
+};
+
 export const fetchData = async (url: string, options: RequestInit): Promise<any> => {
     const response = await fetch(url, options);
     const data = await response.json();
